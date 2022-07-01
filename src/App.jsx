959b9ff8a86b349288d47917e0bfd0ac2c2d074d
wernnon <ruizwernnon@gmail.com>
2022-06-30 20:56:40 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 //App.jsx
 import './App.css'
-import './Button.jsx'
+
 
 function App(){
-  
+  const readmore =[
+    {noticia:"Funcinária que dencuniou procurador diz que agressor 'despresava mulheres' e teme  sair na rua:'Muito medo'"},
+    {noticia:"Imposto de Renda 2022: Receita abre consultas ao 2º lote de restituição nesta  quinta; veja se você está no grupo"},
+    {noticia:"Governo opera pra evitar CPI do MEC, que ganhou força após prisão prisão de Milton Ribeiro"},
+    {noticia:"Em Itajubá, assaltantes armados atacam agência da Caixa Econômica Federal;ficam feridos"},
+    {noticia:"Almir Sater é 'parado em blitz' e policiais tietam cantor: ' senhor é dono dessa chalana?'"}
+  ]
+  const lista = readmore.map(
+    (r)=>
+    <p>
+       {r.noticia}
+    </p> 
+  )
 return(
-<div>
-  
-</div>
+    
+    <div>
+ 
+            {lista}  
+
+    </div>
 )
 }
 export default App;
